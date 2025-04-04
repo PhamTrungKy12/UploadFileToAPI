8e3b74c456b62404243257091f95e957136554c6
Pham Trung Ky <phamtrungky@gmail.com>
2025-04-04 08:30:33 +0700

    commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.uploadfiletoapi"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.uploadfiletoapi"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    implementation(libs.retrofit)
+    implementation(libs.converter)
+    implementation(libs.gson)
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
